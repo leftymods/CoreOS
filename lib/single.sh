6763853f6b23b05e6aee51f3a6bff652032bb3e4
leftymods <leftymods@users.noreply.github.com>
2026-06-11 15:12:52 +0300

    Rebrand: CoreOS/Armbian branding → AtriOS

--- lib/single.sh
+++ lib/single.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-#
-# SPDX-License-Identifier: GPL-2.0
-#
-# Copyright (c) 2013-2026 Igor Pecovnik, igor@armbian.com
-#
-# This file is a part of the Armbian Build Framework
-# https://github.com/armbian/build/
 
-# The Armbian functions require Bash 5.x.
+
 if [[ "${BASH_VERSINFO:-0}" -lt 5 ]]; then
-	echo "Armbian build scripts require Bash 5.x. Go get it..." >&2
+	echo "AtriOS build scripts require Bash 5.x. Go get it..." >&2
 	if [[ "${OSTYPE}" == "darwin"* ]]; then
-		echo "Armbian build scripts require brew to be installed and working on macOS. (old Bash version)" >&2
+		echo "AtriOS build scripts require brew to be installed and working on macOS. (old Bash version)" >&2
 		echo "Please install brew, *restart your terminal*." >&2
 		echo "Then run 'brew install bash coreutils git', *restart your terminal* and then try again." >&2
 		exit 51
@@ -23,13 +16,13 @@ fi
 if [[ "${OSTYPE}" == "darwin"* ]]; then
 	# Don't allow running as root on macOS.
 	if [[ "${EUID}" -eq 0 ]]; then
-		echo "Armbian build scripts do not support running as root on macOS." >&2
+		echo "AtriOS build scripts do not support running as root on macOS." >&2
 		echo "Please run as a normal user." >&2
 		exit 51
 	fi
 
 	if ! command -v brew &> /dev/null; then
-		echo "Armbian build scripts require brew to be installed and working on macOS. (brew not available)" >&2
+		echo "AtriOS build scripts require brew to be installed and working on macOS. (brew not available)" >&2
 		echo "Please install brew, *restart your terminal*." >&2
 		echo "Then run 'brew install bash coreutils git', *restart your terminal* and then try again." >&2
 		exit 51
@@ -37,7 +30,7 @@ if [[ "${OSTYPE}" == "darwin"* ]]; then
 
 	# Run "brew --prefix" to check if brew is working.
 	if ! brew --prefix &> /dev/null; then
-		echo "Armbian build scripts require brew to be installed and working on macOS. (brew --prefix failed)" >&2
+		echo "AtriOS build scripts require brew to be installed and working on macOS. (brew --prefix failed)" >&2
 		echo "Please install brew, *restart your terminal*." >&2
 		echo "Then run 'brew install bash coreutils git', *restart your terminal* and then try again." >&2
 		exit 51
@@ -48,7 +41,7 @@ if [[ "${OSTYPE}" == "darwin"* ]]; then
 
 	# Make sure realpath is available via brew's coreutils, under ${brew_prefix}
 	if ! command -v "${brew_prefix}/opt/coreutils/libexec/gnubin/realpath" &> /dev/null; then
-		echo "Armbian build scripts require realpath to be installed via brew's coreutils on macOS. (realpath not available)" >&2
+		echo "AtriOS build scripts require realpath to be installed via brew's coreutils on macOS. (realpath not available)" >&2
 		echo "Please install brew, *restart your terminal*." >&2
 		echo "Then run 'brew install bash coreutils git', *restart your terminal* and then try again." >&2
 		echo "If that fails, try 'brew reinstall bash coreutils git' and try again." >&2
@@ -63,14 +56,14 @@ if [[ "${OSTYPE}" == "darwin"* ]]; then
 	# Under Darwin/Docker, the "${SRC}" should be under "${HOME}" -- otherwise Docker will not be able to share/mount it.
 	# This is a sanity check to make sure that the user is not trying to build outside of "${HOME}".
 	if [[ "${SRC}" != "${HOME}"* ]]; then
-		echo "Armbian build scripts require the Armbian directory ($SRC) to be under your home directory ($HOME) on macOS." >&2
+		echo "AtriOS build scripts require the AtriOS directory ($SRC) to be under your home directory ($HOME) on macOS." >&2
 		echo "Please clone inside your home directory and try again." >&2
 		exit 52
 	fi
 fi
 
 if [[ -z "$(command -v realpath)" ]]; then
-	echo "Armbian build scripts require coreutils. Go install it." >&2
+	echo "AtriOS build scripts require coreutils. Go install it." >&2
 	exit 53
 fi
 
